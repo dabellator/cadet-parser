dbcfa892eba64eac39223165bd32a952129ad4f9
timcmiller <timcmiller@live.com>
2015-12-15 13:00:04 -0800

    added parser pushing prefix

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require(__dirname + '/lib/parser.js');
+var parser = module.exports = require(__dirname + '/lib/parser.js');
+
+parser(__dirname + '/example/example.md', function(err, data) {
+
+});
